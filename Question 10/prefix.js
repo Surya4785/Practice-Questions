479594cb66509be38d14bf7b03c3159cf9e852ad
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-02-05 14:35:29 +0530

    Add files via upload

---
+++ Question 10/prefix.js
@@ -0,0 +1,17 @@
+//to find longest common prefix.
+function answer(strs) {
+    let pref = strs[0];
+      let prefLen = pref.length;
+  
+      for (let i = 1; i < strs.length; i++) {
+          let s = strs[i];
+          while (pref !== s.substring(0, prefLen)) {
+              prefLen--;
+              if (prefLen === 0) {
+                  return "";
+              }
+              pref = pref.substring(0, prefLen);
+          }
+      }
+    console.log(pref);
+  }
---
+++ Question 10/prefix.py
@@ -0,0 +1,16 @@
+def answer(strs):
+if not strs:
+    return ""
+
+pref = strs[0]
+pref_len = len(pref)
+
+for i in range(1, len(strs)):
+    s = strs[i]
+    while not s.startswith(pref):
+        pref_len -= 1
+        if pref_len == 0:
+            return ""
+        pref = pref[:pref_len]
+
+return pref</code></pre>
