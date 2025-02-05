673697d5ed290c10ffb9f0f47d983b47ae4562ff
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-02-05 14:19:57 +0530

    Python

---
+++ Question 09/palindrome.py
@@ -0,0 +1,9 @@
+def answer(str):
+org = str
+rev = ""    for i in range(len(str) - 1, -1, -1):
+        rev += str[i]
+
+    if org == rev:
+        print("Palindrome")
+    else:
+        print("Not Palindrome")</code></pre>
