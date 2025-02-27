3822a0b6ceee9f2d90b09853f319e043d2b4560a
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-02-27 22:58:56 +0530

    largest string

--- Question 11/largest.py
+++ Question 11/largest.py
@@ -1,10 +1,7 @@
-def answer(num):
-    
+def answer(num): 
     for i in range(len(num) - 1, -1, -1):
         if int(num[i]) % 2 == 1:
             return num[:i + 1]
     
     return ""  # Return an empty string if no odd digit is found
-
-
     print(num)
