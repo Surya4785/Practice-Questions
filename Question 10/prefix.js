53a527e009a8bdf3f0982785d9decf19b46fd37a
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-02-27 23:00:23 +0530

    Longest common prefix

--- Question 10/prefix.js
+++ Question 10/prefix.js
@@ -5,6 +5,7 @@ function answer(strs) {
   
       for (let i = 1; i < strs.length; i++) {
           let s = strs[i];
+          
           while (pref !== s.substring(0, prefLen)) {
               prefLen--;
               if (prefLen === 0) {
