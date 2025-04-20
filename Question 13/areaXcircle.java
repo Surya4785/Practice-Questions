0b3ecae8e7283ae1ca32ff097b3410cb87f0dc96
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-04-20 18:31:23 +0530

    Add files via upload

---
+++ Question 13/areaXcircle.java
@@ -0,0 +1,8 @@
+public class areaXcircle {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double r = 5;
+        double areaXcircle = pi*r*r;
+        System.out.println("The area of circle is : " +areaXcircle);
+    }  
+}
