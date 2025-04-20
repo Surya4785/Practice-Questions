12ce75c57783ffd0a6cb63f7a55be7e7093c01ea
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-04-20 18:36:16 +0530

    Add files via upload

---
+++ Question 13/percentcalculation.java
@@ -0,0 +1,7 @@
+public class percentcalculation {
+    public static void main(String[] args) {
+        double x1 = 90, x2 = 91, x3 = 92, x4 = 93, x5 = 94;
+        double y = (x1+x2+x3+x4+x5)/5;
+        System.out.println(y);
+    }   
+}
