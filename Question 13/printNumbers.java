f06a44cea292d4709bcd89aef21b0b000d10ac47
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-04-20 18:36:40 +0530

    Add files via upload

---
+++ Question 13/printNumbers.java
@@ -0,0 +1,6 @@
+public class printNumbers{
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("This is my first Program  of java");
+    }
+}
