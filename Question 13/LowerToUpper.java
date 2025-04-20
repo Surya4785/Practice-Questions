7af82ecee0341f798d43e78580c65cc42e5eff7a
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-04-20 18:35:04 +0530

    Add files via upload

---
+++ Question 13/LowerToUpper.java
@@ -0,0 +1,13 @@
+public class LowerToUpper {
+    public static void main(String[] args) {
+        String lowerCaseString = "hello world";
+
+        
+        String upperCaseString = lowerCaseString.toUpperCase();
+
+        
+        System.out.println("Original String: " + lowerCaseString);
+        System.out.println("Uppercase String: " + upperCaseString);
+    }
+}
+
