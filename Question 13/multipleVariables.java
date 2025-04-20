ab8c74d373575f47d52f056009a66ff8666e53ef
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-04-20 18:35:29 +0530

    Add files via upload

---
+++ Question 13/multipleVariables.java
@@ -0,0 +1,12 @@
+public class multipleVariables {
+    public static void main(String[] args) {
+        int x=5, y=8, z=9;
+        System.out.print("The value of x is: ");
+        System.out.println(x);
+        System.out.print("The value of y is: ");
+        System.out.println(y);
+        System.out.print("The value of z is: ");
+        System.out.println(z);
+        System.out.println("Total value is :"+x+y+z);
+    }
+}
