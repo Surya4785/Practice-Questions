0313d28d7a354c92b03518a5718112f7b29f9c3a
Surya Prakash Yadav <suryaprakash8707yadav@gmail.com>
2025-04-20 18:37:56 +0530

    Add files via upload

---
+++ Question 13/variables.java
@@ -0,0 +1,12 @@
+public class variables {
+    public static void main(String[] args) {
+        int x;
+        x = 500;
+        System.out.println(x);
+        System.out.println("This is the amount I wanna charge you.");
+        x = x+5;
+        System.out.println(x);
+        x = x-10;
+        System.out.println(x);
+    } 
+}
